d52bdea2c9df1a6c7b39725252da90ee0c929b37
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-07-03 07:50:21 -0400

    Adding Some tests for importing Block Data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
+import TableBlock2 from './class-representation/TableBlock2';
 import './App.css';
+
+
+const indata = {
+  entryId: '1',
+  classesName: 'SENG 2000',
+  descriptions: [
+    'Advanced Data Structures and Algorithms'
+  ] 
+}
+const blockData = {
+  name: 'SENG 2000',
+  desc1: 'Advanced Data Structures and Algorithms'
+}
+const blockData2 = [
+  {
+    name: 'SENG 2000',
+    desc1: 'Advanced Data Structures and Algorithms' 
+  },
+  {
+    name: 'SENG 1000',
+    desc1: 'Software Engineering Foundations'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+    <div className="ui grid">
+      <div className="ui twelve wide column">
+        <div className="App">
+          <h2>Classes</h2>
+          <table class="ui table celled">
+            {/* <tr>
+              <TableBlock2
+                blockData={blockData}
+              />
+            </tr> */}
+          
+          <TableBlock2 blockData={blockData2} />
+          </table>
+        </div>
+      </div>
+      <div className="ui four wide column">
+        Logo goes here.
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/class-representation/ClassPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ClassPage = ({name}) => {
+  return (
+    <div>{name}</div>
+  )
+}
+
+export default ClassPage
