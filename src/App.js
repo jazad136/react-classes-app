6887ee74c3e96273bae0d81fb1be64c2828fc335
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-07-12 17:12:56 -0400

    Add Basic Classes Page Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
+import {Grid, Segment, Container} from 'semantic-ui-react';
 import TableBlock2 from './class-representation/TableBlock2';
+import ClassPage from './class-representation/ClassPage'
 import './App.css';
 
 
@@ -26,28 +28,42 @@ const blockData2 = [
   }
 ]
 
-function App() {
+const getBlockData = () => { 
+  return ''
+}
+const App = () => {
+  const [currentClass, setClasses] = useState([]);
+  const images = [
+    "index-images/algos-rslide.png"
+  ]
+  const imageCaptions = [
+    "",
+    "Live Demonstrations",
+    "Running Time Analysis",
+  ] 
+  const descriptions = [
+    "Advanced Data Structures and Algorithms",
+    "In this sorting demo, a player holds in their left hand a deck of unsorted \"cards\" with numbers, and his task is to move them to the right hand one at a time in sorted order.",
+    "Simpler algorithms were given a treatment that analyze expected amount of time the algorithm should run in the best and worst cases. Insertion sort the element that is least in the leftmost spot, with \"L\" serving as the algorithm's output list by the end.",
+    "We explore dynamic"
+  ]
   return (
     <div className="ui container">
-    <div className="ui grid">
-      <div className="ui twelve wide column">
-        <div className="App">
-          <h2>Classes</h2>
-          <table class="ui table celled">
-            {/* <tr>
-              <TableBlock2
-                blockData={blockData}
-              />
-            </tr> */}
-          
-          <TableBlock2 blockData={blockData2} />
-          </table>
+      <div className="ui grid">
+        <div className="ui twelve wide column">
+          <div className="App">
+            <h2>Classes</h2>
+            {/* <table class="ui table celled"> */}
+
+            <ClassPage 
+            name="SENG 2000"
+            descriptions={descriptions} 
+            imageCaptions={imageCaptions}/>
+            {/* </table> */}
+          </div>
         </div>
+        <div className="ui four wide column">Logo goes here.</div>
       </div>
-      <div className="ui four wide column">
-        Logo goes here.
-      </div>
-    </div>
     </div>
   );
 }
