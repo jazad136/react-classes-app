75ecb589be6f6551c2d24fdd8387dc7b55dd71c6
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-07-15 22:49:53 -0400

    Change App Test.js and remove App.css

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Classes';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Classes />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
